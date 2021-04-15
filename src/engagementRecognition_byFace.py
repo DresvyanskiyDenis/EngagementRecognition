9bbd1e43b81a1ef9e2992a8d9b9b7068b0c9103a
DD <ecodenis@yandex.ru>
2021-04-15 16:09:27 +0200

    Functions extracted faces from images was changed - counters are added (and print functions as well) , and the situation, when no bboxes are found (or one of coordinate is negative), now are processed.

--- src/engagementRecognition_byFace.py
+++ src/engagementRecognition_byFace.py
@@ -3,7 +3,7 @@
 """
 TODO: write description of module
 """
-
+import time
 from typing import Optional, Tuple
 
 import numpy as np
@@ -29,6 +29,9 @@ def extract_faces_from_dir(path_to_dir:str, output_dir:str,detector:object, resi
         img=load_image(os.path.join(path_to_dir,image_filename))
         # calculate bounding boxes for the most confident person via provided detector
         bbox=recognize_the_most_confident_person_retinaFace(img, detector)
+        bbox=tuple(max(0, _) for _ in bbox)
+        if bbox is None or len(bbox)==0:
+            continue
         # extract face from image according bbox
         face_img=extract_face_according_bbox(img, bbox) # it is in RGB format now
         # resize if needed
@@ -44,11 +47,13 @@ def extract_faces_from_all_subdirectories_in_directory(path_to_dir:str, path_to_
     # get subdirectories from provided dir
     subdirectories=os.listdir(path_to_dir)
     # load retinaFace face detector
-    detector=load_and_prepare_detector_retinaFace()
+    detector=load_and_prepare_detector_retinaFace('retinaface_mnet025_v2')
     # create output path, if it is not existed
     if not os.path.exists(path_to_output):
         os.makedirs(path_to_output, exist_ok=True)
     # go over all subdirectories
+    counter=0
+    overall=len(subdirectories)
     for subdirectory in subdirectories:
         # inside every subdirectory the subdirectory_frames subsubdirectory is localed. There are frames extracted
         # from video and we need to extract faces from every image
@@ -58,9 +63,15 @@ def extract_faces_from_all_subdirectories_in_directory(path_to_dir:str, path_to_
         output_dir_name=os.path.join(path_to_output, subdirectory)
         if not os.path.exists(output_dir_name):
             os.makedirs(output_dir_name, exist_ok=True)
+        else:
+            counter += 1
+            continue
         # extract faces from defined subdirectory
+        start=time.time()
         extract_faces_from_dir(path_to_dir=os.path.join(path_to_dir, subdirectory, subdirectory_with_frames),
                                output_dir=output_dir_name, detector=detector, resize=resize)
+        print('processed:%i, overall:%i, time:%f'%(counter, overall, time.time()-start))
+        counter+=1
 
 
 

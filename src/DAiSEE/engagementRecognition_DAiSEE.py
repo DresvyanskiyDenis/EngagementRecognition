d9b5096144ee4054e9b6fa282e189ca1f2983248
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2021-05-12 18:00:29 +0200

    All preprocessing function was moved to preprocessing_DAiSEE.py file.

--- src/DAiSEE/engagementRecognition_DAiSEE.py
+++ src/DAiSEE/engagementRecognition_DAiSEE.py
@@ -38,63 +38,6 @@ class Label(NamedTuple):
 
 
 
-"""def extract_faces_from_dir(path_to_dir:str, output_dir:str,detector:object, resize:Optional[Tuple[int,int]])->None:
-    # TODO: add description
-    # get filenames of all images in dir
-    image_filenames=os.listdir(path_to_dir)
-    # create, if necessary, output dir
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir, exist_ok=True)
-    # extract faces from each image
-    for image_filename in image_filenames:
-        # load image by PIL.Image
-        img=load_image(os.path.join(path_to_dir,image_filename))
-        # calculate bounding boxes for the most confident person via provided detector
-        bbox=recognize_the_most_confident_person_retinaFace(img, detector)
-        bbox=tuple(max(0, _) for _ in bbox)
-        if bbox is None or len(bbox)==0:
-            continue
-        # extract face from image according bbox
-        face_img=extract_face_according_bbox(img, bbox) # it is in RGB format now
-        # resize if needed
-        if not resize is None:
-            face_img=resize_image(face_img, resize)
-        # save
-        save_image(face_img, path_to_output=os.path.join(output_dir, image_filename))
-
-
-def extract_faces_from_all_subdirectories_in_directory(path_to_dir:str, path_to_output:str,
-                                                       resize:Optional[Tuple[int,int]])-> None:
-    # TODO: add description
-    # get subdirectories from provided dir
-    subdirectories=os.listdir(path_to_dir)
-    # load retinaFace face detector
-    detector=load_and_prepare_detector_retinaFace('retinaface_mnet025_v2')
-    # create output path, if it is not existed
-    if not os.path.exists(path_to_output):
-        os.makedirs(path_to_output, exist_ok=True)
-    # go over all subdirectories
-    counter=0
-    overall=len(subdirectories)
-    for subdirectory in subdirectories:
-        # inside every subdirectory the subdirectory_frames subsubdirectory is localed. There are frames extracted
-        # from video and we need to extract faces from every image
-        # construct subdirectory_with_frames variable, which is name of subsubdirectory
-        subdirectory_with_frames=subdirectory+'_frames'
-        # create name of directory and the directory itself, in which the faces of images will be extracted
-        output_dir_name=os.path.join(path_to_output, subdirectory)
-        if not os.path.exists(output_dir_name):
-            os.makedirs(output_dir_name, exist_ok=True)
-        else:
-            counter += 1
-            continue
-        # extract faces from defined subdirectory
-        start=time.time()
-        extract_faces_from_dir(path_to_dir=os.path.join(path_to_dir, subdirectory, subdirectory_with_frames),
-                               output_dir=output_dir_name, detector=detector, resize=resize)
-        print('processed:%i, overall:%i, time:%f'%(counter, overall, time.time()-start))
-        counter+=1"""
-
 def sort_images_according_their_class(path_to_images:str, output_path:str, path_to_labels:str):
     dict_labels=load_labels_to_dict(path_to_labels)
     dirs_with_images=os.listdir(path_to_images)

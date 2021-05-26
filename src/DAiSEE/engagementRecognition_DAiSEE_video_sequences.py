18c1dc71fffd13217040e76a1c01edd03b8faa52
Denis <ecodenis@yandex.ru>
2021-05-26 20:45:54 +0200

    Script for training model with local and global attention is written. Now it is the test time

--- src/DAiSEE/engagementRecognition_DAiSEE_video_sequences.py
+++ src/DAiSEE/engagementRecognition_DAiSEE_video_sequences.py
@@ -16,7 +16,7 @@ import tensorflow as tf
 from sklearn.metrics import recall_score, f1_score, accuracy_score
 from sklearn.utils import class_weight
 
-from tensorflow_utils.Layers import Non_local_block_multi_head
+from tensorflow_utils.Layers import Non_local_block_multi_head, Self_attention_non_local_block
 from tensorflow_utils.keras_datagenerators.ImageDataLoader import ImageDataLoader
 from tensorflow_utils.keras_datagenerators.ImageDataLoader_multilabel import ImageDataLoader_multilabel
 from tensorflow_utils.keras_datagenerators.ImageDataPreprocessor import ImageDataPreprocessor
@@ -110,57 +110,35 @@ def form_dataframe_of_relative_paths_to_data_with_multilabels(path_to_data:str, 
         df_with_relative_paths_and_labels=df_with_relative_paths_and_labels.append(tmp_df)
     return df_with_relative_paths_and_labels
 
-
-def get_modified_VGGFace2_resnet_model(dense_neurons_after_conv: Tuple[int,...],
-                                       dropout: float = 0.3,
-                                       regularization:Optional[tf.keras.regularizers.Regularizer]=None,
-                                       output_neurons: Union[Tuple[int,...], int] = 7, pooling_at_the_end: Optional[str] = None,
-                                       pretrained: bool = True,
-                                       path_to_weights: Optional[str] = None,
-                                       multi_head_attention:bool=True) -> tf.keras.Model:
-    pretrained_VGGFace2 = _get_pretrained_VGGFace2_model(path_to_weights, pretrained=pretrained)
-    x=pretrained_VGGFace2.get_layer('activation_48').output
-    if multi_head_attention:
-        x = Non_local_block_multi_head(num_heads=4,  output_channels=1024,
+def construct_model(input_shape:Tuple[int,...],path_to_VGGFace_weights:str, num_classes:int)->tf.keras.Model:
+    # load VGGFace2 and take all model up to last conv layer
+    pretrained_VGGFace2 = _get_pretrained_VGGFace2_model(path_to_VGGFace_weights, pretrained=True)
+    # stack on top of it non-local block (multi-head local attention)
+    x = pretrained_VGGFace2.get_layer('activation_48').output # output from last resnet block
+    x = Non_local_block_multi_head(num_heads=4,  output_channels=512,
                  head_output_channels=None,
                  downsize_factor=8,
-                 shortcut_connection=True)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-    # take pooling or not
-    if pooling_at_the_end is not None:
-        if pooling_at_the_end=='avg':
-            x=tf.keras.layers.GlobalAveragePooling2D()(x)
-        elif pooling_at_the_end=='max':
-            x=tf.keras.layers.GlobalMaxPooling2D()(x)
-        else:
-            raise AttributeError('Parameter pooling_at_the_end can be either \'avg\' or \'max\'. Got %s.'%(pooling_at_the_end))
-    # create Dense layers
-    for dense_layer_idx in range(len(dense_neurons_after_conv)-1):
-        num_neurons_on_layer=dense_neurons_after_conv[dense_layer_idx]
-        x = tf.keras.layers.Dense(num_neurons_on_layer, activation='relu', kernel_regularizer=regularization)(x)
-        if dropout:
-            x = tf.keras.layers.Dropout(dropout)(x)
-    # pre-last Dense layer
-    num_neurons_on_layer=dense_neurons_after_conv[-1]
-    x = tf.keras.layers.Dense(num_neurons_on_layer, activation='relu')(x)
-    # If outputs should be several, then create several layers, otherwise one
-    if isinstance(output_neurons, tuple):
-        output_layers=[]
-        for num_output_neurons in output_neurons:
-            if dropout:
-                output_layer_i = tf.keras.layers.Dropout(dropout)(x)
-            output_layer_i = tf.keras.layers.Dense(128, activation='relu')(output_layer_i)
-            output_layer_i=tf.keras.layers.Dense(num_output_neurons, activation='softmax')(output_layer_i)
-            #output_layer_i=tf.keras.layers.Reshape((-1, 1))(output_layer_i)
-            output_layers.append(output_layer_i)
-    else:
-        output_layers = tf.keras.layers.Dense(output_neurons, activation='softmax')(x)
-        # in tf.keras.Model it should be always a list (even when it has only 1 element)
-        output_layers = [output_layers]
-    # create model
-    model=tf.keras.Model(inputs=pretrained_VGGFace2.inputs, outputs=output_layers)
-    del pretrained_VGGFace2
-    return model
+                 shortcut_connection=True,
+                 relative_position_encoding=True)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    # construct model from it
+    VGGFace_with_local_attention=tf.keras.Model(inputs=pretrained_VGGFace2.inputs, outputs=[x])
+    # construct sequence-based model from former model
+    input_layer=tf.keras.layers.Input(input_shape)
+    time_distributed_local_atten=tf.keras.layers.TimeDistributed(VGGFace_with_local_attention)(input_layer)
+    x = Self_attention_non_local_block(output_channels=512, downsize_factor = 2,
+                 mode='spatio-temporal', name_prefix ="global_attention",
+                 relative_position_encoding=False)(time_distributed_local_atten)
+    # global average pooling
+    x = tf.keras.layers.TimeDistributed(tf.keras.layers.GlobalAveragePooling2D())(x)
+    # now LSTM layers, but it can be simple Dense
+    x = tf.keras.layers.LSTM(256, return_sequences=True)(x)
+    x = tf.keras.layers.LSTM(128, return_sequences=False)(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    output = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+
+    final_model=tf.keras.Model(inputs=[input_layer], outputs=[output])
+    return final_model
 
 
 def train_model(train_generator:Iterable[Tuple[np.ndarray, np.ndarray]], model:tf.keras.Model,
@@ -193,29 +171,25 @@ def generate_paths_with_labels_from_directory(path_to_dir:str, class_name:str, c
 
 
 if __name__ == '__main__':
-    '''path_to_directory_with_frames=r'D:\Databases\DAiSEE\frames'
-    path_to_output_directory=r'D:\Databases\DAiSEE\extracted_faces'
-    resize=(224,224)
-    extract_faces_from_all_subdirectories_in_directory(path_to_directory_with_frames, path_to_output_directory, resize)'''
     # params
-    path_to_train_frames=r'D:\Databases\DAiSEE\DAiSEE\train_preprocessed\extracted_faces'
-    path_to_train_labels=r'D:\Databases\DAiSEE\DAiSEE\Labels\TrainLabels.csv'
-    path_to_dev_frames=r'D:\Databases\DAiSEE\DAiSEE\dev_preprocessed\extracted_faces'
-    path_to_dev_labels=r'D:\Databases\DAiSEE\DAiSEE\Labels\ValidationLabels.csv'
-    '''output_path=r'D:\Databases\DAiSEE\dev_preprocessed\sorted_faces'
-    sort_images_according_their_class(path_to_images=path_to_dev_frames, output_path=output_path,
-                                      path_to_labels=path_to_dev_labels)'''
+    path_to_train_frames=r'E:\Databases\DAiSEE\DAiSEE\train_preprocessed\extracted_faces'
+    path_to_train_labels=r'E:\Databases\DAiSEE\DAiSEE\Labels\TrainLabels.csv'
+    path_to_dev_frames=r'E:\Databases\DAiSEE\DAiSEE\dev_preprocessed\extracted_faces'
+    path_to_dev_labels=r'E:\Databases\DAiSEE\DAiSEE\Labels\ValidationLabels.csv'
+
+    path_to_save_model_and_results='results'
+
     input_shape=(224,224,3)
     num_classes=4
+    num_frames_in_seq=20
     batch_size=8
-    epochs=30
-    highest_lr=0.0001
-    lowest_lr = 0.00001
+    epochs=50
+    highest_lr=0.0005
+    lowest_lr = 0.00005
     momentum=0.9
-    output_path='results'
     # create output path
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    if not os.path.exists(path_to_save_model_and_results):
+        os.makedirs(path_to_save_model_and_results)
     optimizer=tf.keras.optimizers.SGD(highest_lr, momentum=momentum)
     loss=tf.keras.losses.categorical_crossentropy
     # load labels
@@ -224,9 +198,10 @@ if __name__ == '__main__':
     # form dataframes with relative paths and labels
     labels_train=form_dataframe_of_relative_paths_to_data_with_multilabels(path_to_train_frames, dict_labels_train)
     labels_dev=form_dataframe_of_relative_paths_to_data_with_multilabels(path_to_dev_frames, dict_labels_dev)
-    # add full path to them
+    # add full path to filename
     labels_train['filename']=path_to_train_frames+'\\'+labels_train['filename']
     labels_dev['filename'] = path_to_dev_frames +'\\'+ labels_dev['filename']
+    # convert labels into float32 type
     labels_train['engagement']=labels_train['engagement'].astype('float32')
     labels_train['boredom'] = labels_train['boredom'].astype('float32')
     labels_dev['engagement'] = labels_dev['engagement'].astype('float32')
@@ -235,76 +210,11 @@ if __name__ == '__main__':
     labels_train['frustration'] = labels_train['frustration'].astype('float32')
     labels_dev['confusion'] = labels_dev['confusion'].astype('float32')
     labels_dev['frustration'] = labels_dev['frustration'].astype('float32')
-    # add augmentation data and delete all non-engagement class values
-    """labels_train=labels_train.drop(columns=['boredom', 'confusion','frustration'])
-    labels_dev = labels_dev.drop(columns=['boredom', 'confusion', 'frustration'])
 
-    # add augmented images
-    class_0_augmented=generate_paths_with_labels_from_directory(path_to_dir=r'C:\Databases\DAiSEE\train_preprocessed\augmentation\engagement\0',
-                                                                class_name='engagement',
-                                                                class_value=0)
-    class_1_augmented = generate_paths_with_labels_from_directory(
-        path_to_dir=r'C:\Databases\DAiSEE\train_preprocessed\augmentation\engagement\1',
-        class_name='engagement',
-        class_value=1)
-    labels_train=pd.concat([labels_train, class_0_augmented, class_1_augmented], axis=0)
-    # add SMOTE images
-    class_0_SMOTE=generate_paths_with_labels_from_directory(path_to_dir=r'C:\Databases\DAiSEE\train_preprocessed\SMOTE_images\0',
-                                                                class_name='engagement',
-                                                                class_value=0)
-    class_1_SMOTE = generate_paths_with_labels_from_directory(
-        path_to_dir=r'C:\Databases\DAiSEE\train_preprocessed\SMOTE_images\1',
-        class_name='engagement',
-        class_value=1)
-    labels_train = pd.concat([labels_train, class_0_SMOTE, class_1_SMOTE], axis=0)
-    # turn 4-class task into 2-class task
-    labels_train.loc[(labels_train['engagement'] == 1),'engagement'] = 0
-    labels_train.loc[(labels_train['engagement'] == 2),'engagement'] = 1
-    labels_train.loc[(labels_train['engagement'] == 3),'engagement'] = 1
-
-    labels_dev.loc[(labels_dev['engagement'] == 1),'engagement'] = 0
-    labels_dev.loc[(labels_dev['engagement'] == 2),'engagement'] = 1
-    labels_dev.loc[(labels_dev['engagement'] == 3),'engagement'] = 1
-    num_classes = 2"""
-
-    # class weights
-    """class_weights_engagement=class_weight.compute_class_weight(class_weight='balanced',
-                                                               classes=np.unique(labels_train['engagement']),
-                                                               y=labels_train['engagement'].values.reshape((-1,)))
-    #class_weights_engagement/=class_weights_engagement.sum()
-    class_weights_engagement=dict((i,class_weights_engagement[i]) for i in range(len(class_weights_engagement)))"""
-
-    """class_weights_boredom = class_weight.compute_class_weight(class_weight='balanced',
-                                                                 classes=np.unique(labels_train['boredom']),
-                                                                 y=labels_train['boredom'].values.reshape((-1,)))
-    class_weights_boredom /= class_weights_boredom.sum()
-    #class_weights_boredom = dict((i, class_weights_boredom[i]) for i in range(len(class_weights_boredom)))
-
-    class_weights_confusion = class_weight.compute_class_weight(class_weight='balanced',
-                                                                 classes=np.unique(labels_train['confusion']),
-                                                                 y=labels_train['confusion'].values.reshape((-1,)))
-    class_weights_confusion /= class_weights_confusion.sum()
-    #class_weights_confusion = dict((i, class_weights_confusion[i]) for i in range(len(class_weights_confusion)))
-
-    class_weights_frustration = class_weight.compute_class_weight(class_weight='balanced',
-                                                                 classes=np.unique(labels_train['frustration']),
-                                                                 y=labels_train['frustration'].values.reshape((-1,)))
-    class_weights_frustration /= class_weights_frustration.sum()
-    #class_weights_frustration = dict((i, class_weights_frustration[i]) for i in range(len(class_weights_frustration)))"""
-
-    '''# Make major class less presented
-    labels_train=pd.concat([labels_train[labels_train['class']==0],
-                            labels_train[labels_train['class'] == 1],
-                            labels_train[labels_train['class'] == 2].iloc[::5],
-                            labels_train[labels_train['class'] == 3].iloc[::5]
-                            ])'''
     #labels_train=labels_train.iloc[:6400]
     #labels_dev = labels_dev.iloc[:640]
-    # if we use ImageDataLoader, not multilabel
-    #labels_train.columns=['filename', 'class']
-    #labels_dev.columns = ['filename', 'class']
-    # create generators
-    # change labels_train for class VideoSequenceLoader
+
+    # change labels_train to fit for generator VideoSequenceLoader
     labels_train[['filename', 'frame_num']] = labels_train['filename'].str.rsplit('_',1, expand=True)
     labels_train['frame_num']=labels_train['frame_num'].apply(lambda x: x.split('.')[0])
     labels_train = labels_train.drop(columns=['boredom', 'confusion', 'frustration'])
@@ -312,8 +222,17 @@ if __name__ == '__main__':
     labels_train.columns=['filename', 'frame_num', 'class']
     labels_train['frame_num']=labels_train['frame_num'].astype('int32')
 
+    # change labels_dev to fit for generator VideoSequenceLoader
+    labels_dev[['filename', 'frame_num']] = labels_dev['filename'].str.rsplit('_',1, expand=True)
+    labels_dev['frame_num']=labels_dev['frame_num'].apply(lambda x: x.split('.')[0])
+    labels_dev = labels_dev.drop(columns=['boredom', 'confusion', 'frustration'])
+    labels_dev=labels_dev[['filename', 'frame_num', 'engagement']]
+    labels_dev.columns=['filename', 'frame_num', 'class']
+    labels_dev['frame_num']=labels_dev['frame_num'].astype('int32')
+
+    # create sequence generators
     train_gen=VideoSequenceLoader(paths_with_labels=labels_train, batch_size=batch_size,
-                                  num_frames_in_seq=20, proportion_of_intersection=0.5,
+                                  num_frames_in_seq=num_frames_in_seq, proportion_of_intersection=0.5,
                               preprocess_function=VGGFace2_normalization,
                               num_classes=num_classes,
                  horizontal_flip= 0.1, vertical_flip= 0,
@@ -326,12 +245,69 @@ if __name__ == '__main__':
                  num_pool_workers=2)
 
 
+    dev_gen=VideoSequenceLoader(paths_with_labels=labels_dev, batch_size=batch_size,
+                                  num_frames_in_seq=num_frames_in_seq, proportion_of_intersection=0.5,
+                              preprocess_function=VGGFace2_normalization,
+                              num_classes=num_classes,
+                 horizontal_flip= None, vertical_flip= None,
+                 shift= None,
+                 brightness= None, shearing= None, zooming= None,
+                 random_cropping_out = None, rotation = None,
+                 scaling= None,
+                 channel_random_noise= None, bluring= None,
+                 worse_quality= None,
+                 num_pool_workers=2)
+
+    # construct model
+    model=construct_model(input_shape=(20,224,224,3),
+                          path_to_VGGFace_weights=r'E:\model_weights\resnet50_softmax_dim512\weights.h5',
+                          num_classes=num_classes)
+    # freeze model
+    for i in range(141): # freeze up to 4th block
+        model.layers[i].trainable = False
+    model.summary()
+
+    # create logger
+    logger = open(os.path.join(path_to_save_model_and_results, 'val_logs.txt'), mode='w')
+    logger.close()
+    logger = open(os.path.join(path_to_save_model_and_results, 'val_logs.txt'), mode='a')
+    # write training params and all important information:
+    logger.write('# Train params:\n')
+    logger.write('Database:%s\n'%"DAiSEE")
+    logger.write('Epochs:%i\n'%epochs)
+    logger.write('Highest_lr:%f\n'%highest_lr)
+    logger.write('Lowest_lr:%f\n'%lowest_lr)
+    logger.write('Optimizer:%s\n'%optimizer)
+    logger.write('Loss:%s\n'% loss)
+    logger.write('num_frames_in_seq:%i\n'%num_frames_in_seq)
+    logger.write('Additional info:%s\n'%
+                 'Local with global convolutional attention. Local attention consists of multi head non-local blocks, '
+                  'Global attention is non-local block with mode spatio-temporal (it makes attention across timeline as well)')
+    # create metrics
+    metrics=[tf.keras.metrics.CategoricalAccuracy]
+
+    # create callbacks
+    callbacks = [validation_with_generator_callback_multilabel(dev_gen, metrics=(partial(f1_score, average='macro'),
+                                                                                 accuracy_score,
+                                                                                 partial(recall_score,
+                                                                                         average='macro')),
+                                                               num_label_types=1,
+                                                               num_metric_to_set_weights=1,
+                                                               logger=logger),
+                 get_annealing_LRreduce_callback(highest_lr, lowest_lr, 5)]
 
 
+    # train model
+    train_model(train_generator=train_gen, model=model,
+    optimizer=optimizer, loss=loss,
+    epochs=epochs,
+    val_generator=dev_gen,
+    metrics=metrics,
+    callbacks=callbacks,
+    path_to_save_results=path_to_save_model_and_results,
+    class_weights= None,
+    loss_weights= None)
 
-    for x,y in train_gen:
-        print(x.shape)
-        print(y.shape)
 
 
 

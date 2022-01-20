07e01e505a7cce3e8aeb7e89eaef5759079bb110
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2022-01-20 14:00:18 +0100

    tmp

---
+++ src/NoXi/visual_subsystem/frame_wise_models/VGGFace2_training.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+
+
+def generate_abs_paths_to_images_in_all_dirs():
+    pass
+
+
+def generate_abs_paths_to_images_in_dir():
+    pass
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    pass

0dbc34397e30b2cb639510c36ada2c8f1e75eb40
Denis <ecodenis@yandex.ru>
2022-02-01 20:43:33 +0100

    Tmp point for checking the correctness of the data preparation function.

--- src/NoXi/visual_subsystem/frame_wise_models/VGGFace2_training.py
+++ src/NoXi/visual_subsystem/frame_wise_models/VGGFace2_training.py
@@ -1,5 +1,5 @@
 import copy
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Tuple
 
 import numpy as np
 import pandas as pd
@@ -10,12 +10,11 @@ import glob
 from src.NoXi.preprocessing.data_preprocessing import generate_rel_paths_to_images_in_all_dirs
 from src.NoXi.preprocessing.labels_preprocessing import read_noxi_label_file, transform_time_continuous_to_categorical, \
     clean_labels, average_from_several_labels, load_all_labels_by_paths, transform_all_labels_to_categorical, \
-    combine_path_to_images_with_labels_many_videos
+    combine_path_to_images_with_labels_many_videos, generate_paths_to_labels
 from tensorflow_utils.models.CNN_models import get_modified_VGGFace2_resnet_model
 
 
-
-def create_VGGFace2_model(path_to_weights:str, num_classes:Optional[int]=4)->tf.keras.Model:
+def create_VGGFace2_model(path_to_weights: str, num_classes: Optional[int] = 4) -> tf.keras.Model:
     """Creates the VGGFace2 model and loads weights for it using proviede path.
 
     :param path_to_weights: str
@@ -25,31 +24,56 @@ def create_VGGFace2_model(path_to_weights:str, num_classes:Optional[int]=4)->tf.
     :return: tf.keras.Model
             Created tf.keras.Model with loaded weights.
     """
-    model=get_modified_VGGFace2_resnet_model(dense_neurons_after_conv=(512,),
-                                       dropout= 0.3,
-                                       regularization=tf.keras.regularizers.l2(0.0001),
-                                       output_neurons= num_classes, pooling_at_the_end= 'avg',
-                                       pretrained = True,
-                                       path_to_weights = path_to_weights)
+    model = get_modified_VGGFace2_resnet_model(dense_neurons_after_conv=(512,),
+                                               dropout=0.3,
+                                               regularization=tf.keras.regularizers.l2(0.0001),
+                                               output_neurons=num_classes, pooling_at_the_end='avg',
+                                               pretrained=True,
+                                               path_to_weights=path_to_weights)
     return model
 
-def load_and_preprocess_data(path_to_data:str, path_to_labels:str,
-                             class_barriers:np.array, frame_step:int)->pd.DataFrame:
+
+def load_and_preprocess_data(path_to_data: str, path_to_labels: str,
+                             class_barriers: np.array, frame_step: int) -> Tuple[
+    pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """TODO: complete function
 
     :param path_to_data:
     :param path_to_labels:
     :return:
     """
-    paths_to_frames=generate_rel_paths_to_images_in_all_dirs(path_to_data)
-    paths_to_labels=glob.glob(os.path.join(path_to_labels,'**'))
-    labels=load_all_labels_by_paths(paths_to_labels)
-    labels=transform_all_labels_to_categorical(labels,class_barriers)
-
-    paths_to_frames_with_labels=combine_path_to_images_with_labels_many_videos(paths_to_frames, labels,
-                                                                               frame_step=frame_step)
-    return paths_to_frames_with_labels
-
+    # generate paths to images (data)
+    paths_to_images = generate_rel_paths_to_images_in_all_dirs(path_to_data, image_format="png")
+    # generate paths to train/dev/test labels
+    paths_train_labels = generate_paths_to_labels(os.path.join(path_to_labels, "train"))
+    paths_dev_labels = generate_paths_to_labels(os.path.join(path_to_labels, "dev"))
+    paths_test_labels = generate_paths_to_labels(os.path.join(path_to_labels, "test"))
+    # load labels
+    train_labels = load_all_labels_by_paths(paths_train_labels)
+    dev_labels = load_all_labels_by_paths(paths_dev_labels)
+    test_labels = load_all_labels_by_paths(paths_test_labels)
+    del paths_train_labels, paths_dev_labels, paths_test_labels
+    # transform labels to categorical
+    train_labels = transform_all_labels_to_categorical(train_labels, class_barriers)
+    dev_labels = transform_all_labels_to_categorical(dev_labels, class_barriers)
+    test_labels = transform_all_labels_to_categorical(test_labels, class_barriers)
+    # combine paths to images (data) with labels
+    train_image_paths_and_labels = combine_path_to_images_with_labels_many_videos(paths_with_images=paths_to_images,
+                                                                                  labels=train_labels,
+                                                                                  sample_rate_annotations=25,
+                                                                                  frame_step=5)
+    dev_image_paths_and_labels = combine_path_to_images_with_labels_many_videos(paths_with_images=paths_to_images,
+                                                                                labels=dev_labels,
+                                                                                sample_rate_annotations=25,
+                                                                                frame_step=5)
+    test_image_paths_and_labels = combine_path_to_images_with_labels_many_videos(paths_with_images=paths_to_images,
+                                                                                 labels=test_labels,
+                                                                                 sample_rate_annotations=25,
+                                                                                 frame_step=5)
+    # shuffle train data
+    train_image_paths_and_labels = train_image_paths_and_labels.sample(frac=1).reset_index(drop=True)
+    # done
+    return (train_image_paths_and_labels, dev_image_paths_and_labels, test_image_paths_and_labels)
 
 
 def main():

ff9a36a584c0cc78d417144937c590bc99063c03
Denis <ecodenis@yandex.ru>
2022-04-18 14:53:24 +0200

    The printing of the confusion matrix was added. I have changed the generator to the tensorflow one as well.

--- src/NoXi/visual_subsystem/frame_wise_models/model_validation.py
+++ src/NoXi/visual_subsystem/frame_wise_models/model_validation.py
@@ -3,16 +3,20 @@ sys.path.extend(["/work/home/dsu/datatools/"])
 sys.path.extend(["/work/home/dsu/engagement_recognition_project_server/"])
 
 
+from src.NoXi.visual_subsystem.frame_wise_models.MobileNetv3_training import create_MobileNetv3_model
+from tensorflow_utils.tensorflow_datagenerators.ImageDataLoader_tf2 import get_tensorflow_generator
+from tensorflow_utils.tensorflow_datagenerators.tensorflow_image_preprocessing import preprocess_data_MobileNetv3
+
+
 from src.NoXi.visual_subsystem.frame_wise_models.VGGFace2_training import load_and_preprocess_data, \
     create_VGGFace2_model
 from functools import partial
-from sklearn.metrics import recall_score, precision_score, f1_score
+from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix
 import gc
 import numpy as np
 import pandas as pd
 
 from preprocessing.data_normalizing_utils import VGGFace2_normalization
-from tensorflow_utils.keras_datagenerators.ImageDataLoader import ImageDataLoader
 
 
 def validate_model(model, generator):
@@ -20,13 +24,14 @@ def validate_model(model, generator):
     metrics = {
         'val_recall': partial(recall_score, average='macro'),
         'val_precision': partial(precision_score, average='macro'),
-        'val_f1_score:': partial(f1_score, average='macro')
+        'val_f1_score:': partial(f1_score, average='macro'),
+        'confusion_matrix:': confusion_matrix,
     }
     # make predictions for data from generator and save ground truth labels
     total_predictions = np.zeros((0,))
     total_ground_truth = np.zeros((0,))
-    for x, y in generator:
-        predictions = model.predict(x, batch_size=32)
+    for x, y in generator.as_numpy_iterator():
+        predictions = model.predict(x, batch_size=64)
         predictions = predictions.argmax(axis=-1).reshape((-1,))
         total_predictions = np.append(total_predictions, predictions)
         total_ground_truth = np.append(total_ground_truth, y.argmax(axis=-1).reshape((-1,)))
@@ -41,13 +46,19 @@ def validate_model(model, generator):
     # print results
     for key, value in results.items():
         print("Metric: %s, result:%f"%(key, value))
+    # print confusion matrix separately one more time for better vision
+    print("Confusion matrix")
+    print(results['confusion_matrix'])
 
 
 
 def main():
-    # loading data
+    # params
     frame_step = 5
     path_to_data = "/Noxi_extracted/NoXi/extracted_faces/"
+    path_to_model_weights = ""
+
+    # loading data
     # french data
     path_to_labels_french = "/media/external_hdd_1/NoXi_annotations_reliable_gold_standard_classification_with_additional_train_data/French"
     train_french, dev_french, test_french = load_and_preprocess_data(path_to_data, path_to_labels_french,
@@ -71,28 +82,19 @@ def main():
     del dev_english, dev_french, dev_german
     del test, test_english, test_german, test_french
     gc.collect()
-    pass
 
-    dev_data_loader = ImageDataLoader(paths_with_labels=dev, batch_size=32,
-                                      preprocess_function=VGGFace2_normalization,
-                                      num_classes=5,
-                                      horizontal_flip=0, vertical_flip=0,
-                                      shift=0,
-                                      brightness=0, shearing=0, zooming=0,
-                                      random_cropping_out=0, rotation=0,
-                                      scaling=0,
-                                      channel_random_noise=0, bluring=0,
-                                      worse_quality=0,
-                                      mixup=None,
-                                      prob_factors_for_each_class=None,
-                                      pool_workers=16)
+    dev_data_loader = get_tensorflow_generator(paths_and_labels=dev, batch_size=128,
+                                                 augmentation=False,
+                                                 augmentation_methods=None,
+                                                 preprocessing_function=preprocess_data_MobileNetv3,
+                                                 clip_values=None,
+                                                 cache_loaded_images=False)
 
 
     # model initialization
-    model = create_VGGFace2_model(path_to_weights='/work/home/dsu/VGG_model_weights/resnet50_softmax_dim512/weights.h5',
-                                  num_classes=5)
+    model = create_MobileNetv3_model(num_classes=5)
     # load weights of trained model
-    model.load_weights('/work/home/dsu/model_best_val_recall.h5')
+    model.load_weights(path_to_model_weights)
     # model compilation
     model.compile(loss='categorical_crossentropy', optimizer='Adam')
     model.summary()

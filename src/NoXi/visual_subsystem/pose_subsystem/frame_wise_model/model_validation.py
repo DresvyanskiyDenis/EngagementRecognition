c0bbea457c48ec62dd3434f644d319dc9e04c1a7
Denis <ecodenis@yandex.ru>
2022-08-17 21:35:50 +0200

    tmp

--- src/NoXi/visual_subsystem/pose_subsystem/frame_wise_model/model_validation.py
+++ src/NoXi/visual_subsystem/pose_subsystem/frame_wise_model/model_validation.py
@@ -1,39 +1,26 @@
 import gc
+import glob
 import os
 import sys
-
-from src.NoXi.visual_subsystem.pose_subsystem.frame_wise_model.HRnet_training_wandb import get_data_loaders_from_data
-
 sys.path.extend(["/work/home/dsu/datatools/"])
 sys.path.extend(["/work/home/dsu/engagement_recognition_project_server/"])
 sys.path.extend(["/work/home/dsu/simpleHRNet/"])
 
-from collections import Callable
-from functools import partial
-from typing import Dict, List, Optional
 
-import numpy as np
-import pandas as pd
-import wandb
+
+from src.NoXi.visual_subsystem.pose_subsystem.frame_wise_model.HRnet_training_wandb import get_data_loaders_from_data
+from functools import partial
+
 import torch
 import torchvision.transforms as T
 from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix
-from sklearn.utils import compute_class_weight
-from torchinfo import summary
 
-from decorators.common_decorators import timer
-from pytorch_utils.callbacks import TorchEarlyStopping, TorchMetricEvaluator
-from pytorch_utils.generators.ImageDataGenerator import ImageDataLoader
-from pytorch_utils.generators.pytorch_augmentations import pad_image_random_factor, grayscale_image, \
-    collor_jitter_image_random, gaussian_blur_image_random, random_perspective_image, random_rotation_image, \
-    random_crop_image, random_posterize_image, random_adjust_sharpness_image, random_equalize_image, \
-    random_horizontal_flip_image, random_vertical_flip_image
-from pytorch_utils.losses import FocalLoss, SoftFocalLoss
+from pytorch_utils.callbacks import TorchMetricEvaluator
 from src.NoXi.visual_subsystem.pose_subsystem.frame_wise_model.HRNet import load_HRNet_model, modified_HRNet
 from src.NoXi.visual_subsystem.pose_subsystem.frame_wise_model.utils import load_NoXi_data_all_languages, \
     convert_image_to_float_and_scale
 
-def validate_model(dataset:torch.utils.data.DataLoader, model:torch.nn.Module)->None:
+def _validate_model_on_dataset(dataset:torch.utils.data.DataLoader, model:torch.nn.Module, device):
     # specify val metrics
     val_metrics = {
         'recall': partial(recall_score, average='macro'),
@@ -41,8 +28,6 @@ def validate_model(dataset:torch.utils.data.DataLoader, model:torch.nn.Module)->
         'f1_score:': partial(f1_score, average='macro'),
         'confusion_matrix': confusion_matrix
     }
-    # specify device
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     # make evaluator, which will be used to evaluate the model
     metric_evaluator = TorchMetricEvaluator(generator = dataset,
                  model=model,
@@ -59,17 +44,18 @@ def validate_model(dataset:torch.utils.data.DataLoader, model:torch.nn.Module)->
 
 
 
-
-
 def main():
     # params
-    BATCH_SIZE=64
-    NUM_CLASSES=5
+    BATCH_SIZE = 64
+    NUM_CLASSES = 5
+    path_to_dir_with_weights = "/work/home/dsu/Model_weights/weights_of_best_models/frame_to_frame_experiments/Pose_model/"
+    paths_to_weights= glob.glob(os.path.join(path_to_dir_with_weights, "*.pt"))
+    # specify device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     # load data
     train, dev, test = load_NoXi_data_all_languages(train_labels_as_categories=False,
                                                     dev_labels_as_categories=False,
                                                     test_labels_as_categories=False)
-
     train_gen, dev_gen, test_gen = get_data_loaders_from_data(train, dev, test, augment=True, augment_prob=0.05,
                                                               batch_size=BATCH_SIZE,
                                                               preprocessing_functions=[T.Resize(size=(256, 256)),
@@ -79,8 +65,29 @@ def main():
                                                                                            std=[0.229, 0.224, 0.225])
                                                                                        ])  # From HRNet
 
-    # create model and load its weights
-
+    for path_to_weights in paths_to_weights:
+        # create model and load its weights
+        HRNet = load_HRNet_model(device="cuda" if torch.cuda.is_available() else "cpu",
+                                 path_to_weights="/work/home/dsu/simpleHRNet/pose_hrnet_w32_256x192.pth")
+        model = modified_HRNet(HRNet, num_classes=NUM_CLASSES)
+        model.load_state_dict(torch.load(path_to_weights))
+        model.to(device)
+        model.eval()
+        # calculate metrics
+        val_metrics = _validate_model_on_dataset(dataset=dev_gen, model=model, device=device)
+        test_metrics = _validate_model_on_dataset(dataset=test_gen, model=model, device=device)
+        # print results
+        print("HRNet model, weights: {}".format(path_to_weights))
+        print("Validation metrics:")
+        for key, value in val_metrics.items():
+            print(key, value)
+        print("Test metrics:")
+        for key, value in test_metrics.items():
+            print(key, value)
+        print("-----------------------------------------------------")
+        # clear RAM
+        gc.collect()
+        torch.cuda.empty_cache()
 
 
 if __name__ == "__main__":

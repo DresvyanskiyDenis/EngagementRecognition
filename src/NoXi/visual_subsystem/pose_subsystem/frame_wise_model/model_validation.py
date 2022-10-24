74d0cee279bc7f2371012cf653ecc9e3ff464f39
Denis <ecodenis@yandex.ru>
2022-10-24 23:06:48 +0200

    Small changes to run cross-corpus experiment

--- src/NoXi/visual_subsystem/pose_subsystem/frame_wise_model/model_validation.py
+++ src/NoXi/visual_subsystem/pose_subsystem/frame_wise_model/model_validation.py
@@ -18,7 +18,8 @@ from sklearn.metrics import recall_score, precision_score, f1_score, confusion_m
 from pytorch_utils.callbacks import TorchMetricEvaluator
 from src.NoXi.visual_subsystem.pose_subsystem.frame_wise_model.HRNet import load_HRNet_model, modified_HRNet
 from src.NoXi.visual_subsystem.pose_subsystem.frame_wise_model.utils import load_NoXi_data_all_languages, \
-    convert_image_to_float_and_scale
+    convert_image_to_float_and_scale, load_NoXi_data_cross_corpus
+
 
 def _validate_model_on_dataset(dataset:torch.utils.data.DataLoader, model:torch.nn.Module, device):
     # specify val metrics
@@ -46,18 +47,19 @@ def _validate_model_on_dataset(dataset:torch.utils.data.DataLoader, model:torch.
 
 
 def main():
+    language = "german"
     print("Start...")
     # params
     BATCH_SIZE = 64
     NUM_CLASSES = 5
-    path_to_dir_with_weights = "/work/home/dsu/Model_weights/weights_of_best_models/frame_to_frame_experiments/Pose_model/All_languages/"
+    path_to_dir_with_weights = "/work/home/dsu/Model_weights/weights_of_best_models/frame_to_frame_experiments/Pose_model/Cross_corpus/"\
+                               +language.capitalize()+"/"
     paths_to_weights= glob.glob(os.path.join(path_to_dir_with_weights, "*.pt"))
     # specify device
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     # load data
-    train, dev, test = load_NoXi_data_all_languages(train_labels_as_categories=False,
-                                                    dev_labels_as_categories=False,
-                                                    test_labels_as_categories=False)
+    train, dev, test = load_NoXi_data_cross_corpus(test_corpus=language, train_labels_as_categories=False,
+                                                    dev_labels_as_categories=False,test_labels_as_categories=False)
     train_gen, dev_gen, test_gen = get_data_loaders_from_data(train, dev, test, augment=True, augment_prob=0.05,
                                                               batch_size=BATCH_SIZE,
                                                               preprocessing_functions=[T.Resize(size=(256, 256)),
@@ -95,4 +97,5 @@ def main():
 
 
 if __name__ == "__main__":
+    print("Cross corpus model evaluation...")
     main()
